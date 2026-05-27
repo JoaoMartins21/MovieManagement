cdcf1e8127e6b237ea64915acb07dd11f17f06e2
JoaoMartins21 <joaoc59557@gmail.com>
2026-05-27 16:43:23 +0100

    Implementar o repositório de filmes e estrutura do domínio

---
+++ MovieManagement.Data/Repositories/MovieRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MovieManagement.Domain.Entities;
+using MovieManagement.Domain.Interfaces;
+
+namespace MovieManagement.Data.Repositories
+{
+    public class MovieRepository : IMovieRepository
+    {
+        private List<Movie> movies = new List<Movie>();
+
+        public void Add(Movie movie)
+        {
+            movies.Add(movie);
+        }
+
+        public List<Movie> GetAll()
+        {
+            return movies;
+        }
+
+        public Movie Get(int id)
+        {
+            return movies.Find(m => m.Id == id);
+        }
+
+        public Movie Update(Movie movie)
+        {
+            Movie existingMovie = Get(movie.Id);
+
+            if (existingMovie != null)
+            {
+                existingMovie.Title = movie.Title;
+                existingMovie.Year = movie.Year;
+                existingMovie.Language = movie.Language;
+                existingMovie.Rating = movie.Rating;
+            }
+            return existingMovie;
+        }
+        public void Delete(int id)
+        {
+            Movie movie = Get(id);
+
+            if (movie != null)
+            {
+                movies.Remove(movie);
+            }
+        }
+    }
+}
---
+++ MovieManagement.Domain/Interfaces/IMovieRepository.cs
@@ -0,0 +1,20 @@
+﻿using MovieManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieManagement.Domain.Interfaces
+{
+    public interface IMovieRepository
+    {
+        void Add(Movie movie);
+        
+        List<Movie> GetAll();
+
+        Movie Get(int id);
+
+        Movie Update(Movie movie);
+
+        void Delete(int id);
+    }
+}
