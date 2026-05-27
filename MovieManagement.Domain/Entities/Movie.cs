abdcf1363e142ec59d1e1b147f404520dacc3d33
JoaoMartins21 <joaoc59557@gmail.com>
2026-05-27 15:56:09 +0100

    Criação da camada Domain e entidade Movie

---
+++ MovieManagement.Domain/Entities/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieManagement.Domain.Entities
+{
+    public class Movie
+    {
+        public int Id {  get; set; }
+
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public string Language { get; set; }
+
+        public double Rating { get; set; }
+
+
+    }
+}
