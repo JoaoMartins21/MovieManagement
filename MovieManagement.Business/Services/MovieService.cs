b13ecf048a0675fc532c791bec15609c89b1f281
JoaoMartins21 <joaoc59557@gmail.com>
2026-06-01 15:18:22 +0100

    Criar camada de negócio e serviço de filmes

---
+++ MovieManagement.Business/Services/MovieService.cs
@@ -0,0 +1,42 @@
+﻿using MovieManagement.Domain.Entities;
+using MovieManagement.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieManagement.Business.Services
+{
+    internal class MovieService
+    {
+        private IMovieRepository repository;
+
+        public MovieService(IMovieRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public void AddMovie(Movie movie)
+        {
+            if (string.IsNullOrWhiteSpace(movie.Title))
+            {
+                throw new Exception("O título é obrigatório.");
+            }
+
+            if (movie.Rating < 0 || movie.Rating > 5)
+            {
+                throw new Exception("A classificação deve estar entre 0 e 5.");
+            }
+
+            repository.Add(movie);
+
+            foreach (Movie existingMovie in repository.GetAll())
+            {
+                if (existingMovie.Title.ToLower() == movie.Title.ToLower())
+                {
+                    throw new Exception("Já existe um filme com esse título.");
+                }
+            }
+        }
+
+    }
+}
